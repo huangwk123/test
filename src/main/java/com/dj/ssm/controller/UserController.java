5d582982eed08bc2fb9b37483925ece18c65b3f9
hwk <hwk13453757019@163.com>
2020-07-15 19:38:31 +0800

    commit aaaa

--- src/main/java/com/dj/ssm/controller/UserController.java
+++ src/main/java/com/dj/ssm/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    String aa = "aaaa";
     @Autowired
     private UserService userService;
 
